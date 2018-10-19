cb414187f148fe88fdb23995b8c08e98831cbce1
Haijun Su <navysu@gmail.com>
2018-10-19 14:32:28 +0000

    ansible problem

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,8 @@ sudo chown root:root ${USER}
 sudo mv ${USER} /etc/sudoers.d/
 
 # install ansible and git
+sudo apt-add-repository ppa:ansible/ansible
+# if report issue for install ansible as "Fails due to SSHPASS dependency", uncomment next line to fix it.
+# sudo add-apt-repository universe
 sudo apt update
 sudo apt install ansible git -y
